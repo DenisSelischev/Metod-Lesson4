5e70e2164829f5879bf3ccaacaf585c1186d6556
Denis <Deni-SelischeV23@yandex.ru>
2023-02-25 20:39:33 +0300

    Solved 3 task

---
+++ Task003/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите метод, который задаёт массив из N элементов и выводит их на экран.
+
+int lenArray = InputInt("Введите длинну массива: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
+
+int InputInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
