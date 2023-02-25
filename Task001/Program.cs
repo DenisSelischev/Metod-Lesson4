5018736eb21a9457b57792d537d133ab88ab9996
Denis <Deni-SelischeV23@yandex.ru>
2023-02-25 20:25:20 +0300

    Solved 1 task

---
+++ Task001/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int numberA = InputInt("Введите число A: ");
+int numberB = InputInt("Введите число B: ");
+Input(numberA, numberB);
+
+void Input(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+
+int InputInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
